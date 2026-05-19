3f9df42ff2be3131599f9d739476d6cc03304cb1
Yicong Chen <chen.yicong@outlook.com>
2026-05-19 10:41:38 +0200

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,25 @@
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://chinezenopfietspad.pages.dev',
+  site: 'https://chinezenopfietspad.nl',
   base: '/',
   output: 'static',
   trailingSlash: 'ignore',
-  integrations: [svelte()],
+  integrations: [
+    svelte(),
+    sitemap({
+      i18n: {
+        defaultLocale: 'en',
+        locales: {
+          en: 'en',
+          nl: 'nl',
+          zh: 'zh',
+        },
+      },
+    }),
+  ],
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'nl', 'zh'],
